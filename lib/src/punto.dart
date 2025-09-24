d30b6045573e9162b5d97af0f3180cab02a24424
Enrique <proenrique4@gmail.com>
2025-09-24 13:53:53 -0700

    tests

--- lib/src/punto.dart
+++ lib/src/punto.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Punto {
 final int columna;
 final int fila;
@@ -8,6 +10,13 @@ Punto({required this.columna, required this.fila});
 
 
 int distancia(Punto otro){
+int x = columna - otro.columna;
+int y = fila - otro.fila;
+
+num hipo = pow(x,2) + pow(y,2);
+num raiz = sqrt(hipo);
+return raiz.toInt();
+
 
 
 
--- test/src/punto_test.dart
+++ test/src/punto_test.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-import 'package:batallanaval';
+import 'package:batallanaval/src/punto.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -7,8 +7,32 @@ void main() {
 
     Punto p1 = Punto(columna: 1, fila: 1);
     Punto p2 = Punto(columna: 2, fila: 1);
-    expect(p1.distancia(p2),p2.disctancia(p1));
+      expect(p1.distancia(p2), equals(1));
   });
+
+
+    test('ddeben de estar a 2 cordenadas de distancia', () {
+
+    Punto p1 = Punto(columna: 1, fila: 1);
+      Punto p2 = Punto(columna: 3, fila: 1);
+     expect(p1.distancia(p2), equals(2));
+  });
+
+test('ddeben de estar a 2 cordenadas de distancia', () {
+
+    Punto p1 = Punto(columna: 1, fila: 1);
+    Punto p2 = Punto(columna: 3, fila: 1);
+    expect(p1.distancia(p2), equals(2));
+  });  
+  
+
+  test('1,1', () {
+
+    Punto p1 = Punto(columna: 1, fila: 1);
+    Punto p2 = Punto(columna: 3, fila: 1);
+    expect(p1.distancia(p2), equals(2));
+  });  
+  
 
 
 }
